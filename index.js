dd960348636b44ce25cc78ba9ed5f8fc9319e72f
Aniketyadav44 <aniani4848@gmail.com>
2023-02-22 10:56:36 +0530

    f

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import * as IPFS from 'ipfs-core'
 import multer from 'multer'
-import busboy from 'connect-busboy'
+import bodyParser from 'body-parser'
 
 const ipfs = await IPFS.create()
 const app = express()
+const upload = multer()
 
 app.use(express.json())
-app.use(busboy())
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get('/',(req,res)=>{
     return res.send('Welcome to this IPFS app')
@@ -17,17 +18,19 @@ app.listen(3000, ()=>{
     console.log('Server started at port 3000') 
 })
 
-app.post('/upload', async(req,res)=>{
+app.post('/upload', upload.single("file"), async(req,res)=>{
     // const { headers, files } = req;
     // const { buffer, originalname: filename } = files[0];
 
     var fileHash = "sample"
-    req.busboy.on('file', async (fieldname, file, filename) => {
-        console.log(file)
-        console.log("Uploading: " + filename); 
-        fileHash = await addFile(file)
-    });
+    // req.busboy.on('file', async (fieldname, file, filename) => {
+    //     console.log(file)
+    //     console.log("Uploading: " + filename); 
+    //     fileHash = await addFile(file)
+    // });
     // const fileHash = await addFile(buffer)
+    const data = req.file
+    console.log(data)
     return res.send(fileHash)
 })
 
