e8567ebb83b4eb0d00bf5bf9662c80b49583e364
Aniketyadav44 <aniani4848@gmail.com>
2023-02-23 17:00:35 +0530

    added auth

--- index.js
+++ index.js
@@ -12,15 +12,20 @@ const upload = multer()
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
+//reading api key variable
+var serverApiKey = fs.readFileSync("./api")
+
+//encryption and decryption variables
+var algorithm = "aes-192-cbc";
+var buff = fs.readFileSync("./test")
+
+//home get route
 app.get('/',(req,res)=>{
     return res.send('Welcome to this IPFS app')
 })
 
-app.listen(3000, ()=>{
-    console.log('Server started at port 3000') 
-})
-
-app.post('/upload', upload.single("file"), async(req,res)=>{
+//upload post route to upload to ipfs
+app.post('/api/upload', authenticateKey, upload.single("file"), async(req,res)=>{
     const { buffer, originalname: filename } = req.file;
     const secretKey = req.body.secretKey
 
@@ -32,25 +37,8 @@ app.post('/upload', upload.single("file"), async(req,res)=>{
     return res.send({"encryptedCID":encryptedCID,"size": fileHash["size"],"mode": fileHash["mode"],})
 })
 
-//uploading file to ipfs
-const addFile = async({content})=>{
-    const data = await ipfs.add(content)
-    return data
-}
-
-//encryption and decryption service
-var algorithm = "aes-192-cbc";
-var buff = fs.readFileSync("./test")
-const encrypt = (secretKey, cid)=>{
-    const key = crypto.scryptSync(secretKey, 'salt', 24)
-    
-    const cipher = crypto.createCipheriv(algorithm, key, buff)
-    var encryptedCID = cipher.update(cid, 'utf8', 'hex') + cipher.final('hex')
-    return encryptedCID
-}
-
-//decrypt route
-app.post('/decrypt', upload.any(), async(req,res)=>{
+//decryptio route
+app.post('/api/decrypt', authenticateKey, upload.any(), async(req,res)=>{
     try{
         const secretKey = req.body.secretKey
         const encrypted = req.body.encrypted
@@ -64,6 +52,36 @@ app.post('/decrypt', upload.any(), async(req,res)=>{
         if(err.code=="ERR_OSSL_BAD_DECRYPT")
         return res.status(401).send({"error":"INVALID_KEY"})
     }
+})
+
+//uploading function file to ipfs
+const addFile = async({content})=>{
+    const data = await ipfs.add(content)
+    return data
+}
+
+//encryption function to encrypt cid
+const encrypt = (secretKey, cid)=>{
+    const key = crypto.scryptSync(secretKey, 'salt', 24)
+    
+    const cipher = crypto.createCipheriv(algorithm, key, buff)
+    var encryptedCID = cipher.update(cid, 'utf8', 'hex') + cipher.final('hex')
+    return encryptedCID
+}
+
+//api key authentication function
+const authenticateKey = (req, res, next)=>{
+    let apiKey = req.header('x-api-key')
+    if(apiKey==serverApiKey){
+        next()
+    }else{
+        res.status(403).send({"error":"INVALID_API_KEY"})
+    }
+}
+
+//listening on port 3000
+app.listen(3000, ()=>{
+    console.log('Server started at port 3000') 
 })
 
 
